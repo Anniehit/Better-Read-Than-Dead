6b38f9cfd70a9560b5d90b983be1db6033381a2e
Annie <gjhwa11@gmail.com>
2021-03-07 21:04:19 -0500

    feature(oauth): creating auth routes for google

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,6 @@
 const db = require('./db')
 
 // register models
-const {Admin, User, Product, Tag} = require('./models')
+const {User, Product, Tag} = require('./models')
 
-module.exports = {db, Admin, User, Product, Tag}
+module.exports = {db, User, Product, Tag}
